fe184f065ed54347e82925870566e20b8f0a04ca
ZhiangChen <zxc251@case.edu>
2016-07-25 15:34:07 -0400

    cropped images

--- data_preprocess/data_pp/src/label_images.py
+++ data_preprocess/data_pp/src/label_images.py
@@ -13,8 +13,13 @@ from scipy import ndimage
 from six.moves import cPickle as pickle
 
 pixel_depth = 225.0
-image_size = 200
+image_size = 80
 num_image = 0
+
+lf_x = 30
+lf_y = 120
+rt_x = lf_x + image_size
+rt_y = lf_y + image_size
 
 labels = dict()
 label_num = 0
@@ -38,6 +43,8 @@ for name in files:
 	debris = name.split('.')
 	if debris[-1] != 'bmp':
 		continue
+	elif name.startswith('cropped'):
+		continue
 	else:
 		num_images += 1
 print("There are %d images to label" %num_images)
@@ -49,10 +56,16 @@ for name in files:
 	debris = name.split('.')
 	if debris[-1] != 'bmp':
 		continue
+	elif name.startswith('cropped'):
+		continue
 	else:
 		image_file = os.path.join(wd, name)
-		img = Image.open(image_file)
+		img0 = Image.open(image_file)
+		img = img0.crop((lf_x,lf_y,rt_x,rt_y))
 		img.show()
+		img.save(wd+"/cropped_"+name)
+		image_file = os.path.join(wd, "cropped_"+name)
+
 		image_data = (ndimage.imread(image_file).astype(float) - pixel_depth / 2) / pixel_depth
 		if image_data.shape != (image_size, image_size):
 			raise Exception('Unexpected image shape: %s' % str(image_data.shape))
