39302a85fc4a8b3fb2b02abd31b6bbbc2560a570
ZhiangChen <zxc251@case.edu>
2016-07-25 11:41:17 -0400

    data preprocess

---
+++ data_preprocess/data_pp/src/label_images.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+'''
+Zhiang Chen
+July,2016
+'''
+
+"Label the depth images, and store them as the format of 3D volume, which is consistent with the input of convnet"
+
+import os
+import numpy as np
+from PIL import Image
+from scipy import ndimage
+from six.moves import cPickle as pickle
+
+pixel_depth = 225.0
+image_size = 200
+num_image = 0
+
+labels = dict()
+label_num = 0
+
+wd = os.getcwd()
+print("Current directory is \""+wd+"\"")
+print("Start to label the images in this directory? (yes/no)")
+cmd = input()
+assert cmd == "yes" or cmd == "no"
+if cmd == "no":
+	print("Input correct directory:")
+	wd = input()
+	assert os.path.isdir(wd)
+
+input("Before labeling, make sure there is no subdirectories in this folder!\nPress Enter to continue")
+
+files = os.listdir(wd)
+
+num_images = 0
+for name in files:
+	debris = name.split('.')
+	if debris[-1] != 'bmp':
+		continue
+	else:
+		num_images += 1
+print("There are %d images to label" %num_images)
+dataset = np.ndarray(shape=(num_images, image_size, image_size),dtype=np.float32)
+labelset = np.ndarray(shape=(num_images, 1),dtype=np.float32)
+
+for name in files:
+	print("--------------------------------")
+	debris = name.split('.')
+	if debris[-1] != 'bmp':
+		continue
+	else:
+		image_file = os.path.join(wd, name)
+		img = Image.open(image_file)
+		img.show()
+		image_data = (ndimage.imread(image_file).astype(float) - pixel_depth / 2) / pixel_depth
+		if image_data.shape != (image_size, image_size):
+			raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+		print("The file name: "+name)
+		label = input("Input the label of the image: ")
+		if label in labels:
+			label_num = labels[label]
+		else:
+			label_num += 1
+			labels.setdefault(label,label_num)
+		dataset[num_image,:,:] = image_data
+		labelset[num_image,:]=label_num
+		num_image += 1
+
+
+print(dataset.shape)
+print(labelset.shape)
+
+data_file = wd + '/depth_data'
+with open(data_file,'wb') as f:
+	save={
+		'dataset': dataset,
+		'labelset':labelset
+	}
+	pickle.dump(save,f,pickle.HIGHEST_PROTOCOL)
+	f.close()
+statinfo = os.stat(data_file)
+print('Compressed data size:', statinfo.st_size)
